32235b06693a49531b64c69a181071f83fb6c896
shivkumar rajmane <153319619+shivkumar-github@users.noreply.github.com>
2026-02-12 16:13:09 +0530

    Revert "Test"

--- client/src/pages/ExperienceDetailsPage.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function ExperienceDetailsPage() {
-  return (
-	<div>ExperienceDetailsPage</div>
-  )
-}
--- server/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { verifyOtp,sendOtp } = require('../controllers/authController');
-const router = express.Router();
-
-router.post('/send-otp', sendOtp);
-
-router.post('/verify-otp', verifyOtp);
-
-module.exports = router;
